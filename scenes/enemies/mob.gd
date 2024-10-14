c67c6a2cce4e063f2431d39a487bfbeabdaf10fc
DCdeBrabander <2249919+DCdeBrabander@users.noreply.github.com>
2024-10-14 23:02:22 +0200

    Tweaked Mob's patrol() logic
    - patrol improved a little bit. not perfect yet but good enough for further development, finally.
    - also update current rotation of mob again :-)
    - Tweaked get_random_patrol_point to use 'padding' offset

--- scenes/enemies/mob.gd
+++ scenes/enemies/mob.gd
@@ -3,11 +3,9 @@ extends CharacterBody2D
 @onready var navigation_agent = $NavigationAgent2D
 
 var movement_delta: float
-@export var movement_speed: float = 4.0
-
+@export var movement_speed: float = 40000.0
 @export var health: int = 1
 @export var score_on_kill: int = 2
-@export var speed = 10
 
 var current_behaviour: Behaviour = Behaviour.PATROL
 var patrol_area: Rect2
@@ -23,6 +21,9 @@ enum Behaviour {
 	STOPPED,
 }
 
+func _exit_tree() -> void:
+	print("Mob is being freed: ", self)
+	
 func _ready() -> void:
 	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
@@ -34,10 +35,7 @@ func _physics_process(delta: float):
 	match current_behaviour:
 		Behaviour.PATROL: patrol()
 		Behaviour.FOLLOW: follow()
-		_: print("no behaviour")
-
-	# Move the character with the computed velocity
-	move_and_slide()
+		_: print("no behaviour")	
 
 func initialize(position: Vector2, behaviour: Behaviour = Behaviour.PATROL) -> CharacterBody2D:
 	self.position = position
@@ -48,29 +46,25 @@ func patrol():
 	if patrol_area == null:
 		print("Patrol area not set for enemy...")
 		return
-
-	if navigation_agent.is_navigation_finished() or navigation_agent.is_target_reached() or velocity == Vector2.ZERO:
-		var patrol_point = get_random_patrol_point() * 64
-		if (patrol_point == Vector2.ZERO):
-			print("shit's zero boss..", patrol_point)
-			
-		# Set a new patrol point when the target is reached
-		navigation_agent.set_target_position(patrol_point)
-		
-		# Calculate movement delta
-		movement_delta = speed * get_process_delta_time()  # Use speed defined in the script
-
-		# Get the next path position from the navigation agent
-		var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-
-		# Calculate the new velocity towards the next path position
-		var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_delta
-		
-		_on_velocity_computed(new_velocity)
 	
+	if navigation_agent.is_navigation_finished() or velocity.is_zero_approx():
+		var patrol_point = get_random_patrol_point() * 64
+		navigation_agent.set_target_position(patrol_point)
+
+	if not navigation_agent.is_target_reachable() or navigation_agent.is_target_reached():
+		var patrol_point = get_random_patrol_point() * 64
+		navigation_agent.set_target_position(patrol_point)
+
+	movement_delta = movement_speed * get_process_delta_time()
+	
+	var new_velocity: Vector2 = global_position.direction_to(navigation_agent.get_next_path_position()) * movement_delta
+	navigation_agent.set_velocity(new_velocity)
+	rotation = global_position.direction_to(navigation_agent.get_next_path_position()).angle()
+
 func _on_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
-	
+	move_and_slide()
+
 func stop():
 	current_behaviour = Behaviour.STOPPED
 	velocity = Vector2.ZERO
@@ -79,9 +73,6 @@ func stop():
 func follow():
 	print("?")
 
-func get_random_patrol_point() -> Vector2:
-	return patrol_area.position + Vector2(randf() * patrol_area.size.x, randf() * patrol_area.size.y)
-	
 func hit(incoming_damage: int):
 	print("HIT")
 	health -= incoming_damage
@@ -108,6 +99,12 @@ func get_minimap_indicator_style() -> Dictionary:
 		"color": Color(0, 1, 0),
 		"animate": "pulse"
 	}
-
-func _exit_tree() -> void:
-	print("Mob is being freed: ", self)
+	
+func get_random_patrol_point(padding: float = 2.0) -> Vector2:
+	var clamped_min = patrol_area.position + Vector2(padding, padding)
+	var clamped_max = patrol_area.position + patrol_area.size - Vector2(padding, padding)
+	return Vector2(
+		randf_range(clamped_min.x, clamped_max.x),
+		randf_range(clamped_min.y, clamped_max.y)
+	)
+	
