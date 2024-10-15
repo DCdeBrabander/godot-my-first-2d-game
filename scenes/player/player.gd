4a823107615d4898b40b8b4d2dae8c3f06ac74ad
DCdeBrabander <2249919+DCdeBrabander@users.noreply.github.com>
2024-10-16 01:04:59 +0200

    Bullet hits enemy again (hit them twice) and added "dash"-mechanic
    - and some minor code changes to Player
      - for ex. general handle_actions_from_input()
      - moved $ (get_node) instances to @onready

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,19 +4,35 @@ extends CharacterBody2D
 @export var bullet_cooldown = 0.25
 @export var bullet: PackedScene
 
+@onready var player_animation = $AnimatedSprite2D
+@onready var player_collision_shape = $CollisionShape2D
+
 signal hit
 
 enum PlayerStates {
 	ALIVE,
 	DEAD
 }
-
+var _current_delta: float
 var screen_size
+
+# Player stuffs
 var can_shoot = true
 var bullet_direction = Vector2(1, 0)
 var last_player_direction = Vector2.ZERO
 var current_player_state = PlayerStates.DEAD
 var flashlight_enabled = true
+
+# dash mechanic
+var is_dashing = false
+var dash_speed = 2000
+var dash_duration = 0.3
+var dash_time_remaining = 0.0
+var dash_cooldown: float = 1.0
+var dash_cooldown_remaining: float = 1.0
+var dash_direction = Vector2.ZERO
+
+const COOLDOWN_THRESHOLD = 0.01
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -28,42 +44,73 @@ func start(_position: Vector2):
 
 func die():
 	current_player_state = PlayerStates.DEAD
-	$CollisionShape2D.set_deferred("disabled", true)
+	player_collision_shape.set_deferred("disabled", true)
 	get_tree().call_group("bullets", "queue_free")
 	hide()
 
 func alive():
 	current_player_state = PlayerStates.ALIVE
-	$CollisionShape2D.disabled = false
+	player_collision_shape.disabled = false
 	show()
 
 func _process(delta: float) -> void:
 	if (current_player_state == PlayerStates.DEAD): return
+	_current_delta = delta
 	
 	var view_direction: Vector2 = get_mouse_direction()
-
-	toggle_flashlight()
-	move_player(delta)
-	rotate_field_of_view(view_direction)
-	set_animation(view_direction)
-		
-	if (is_shooting()): shoot(view_direction)
 	
-func move_player(delta: float):
+	if dash_cooldown_remaining > 0.0:
+		dash_cooldown_remaining -= delta
+	
+	rotate_field_of_view(view_direction)	
+	handle_actions_from_input()
+	move_player()
+	set_animation(view_direction)
+
+func handle_actions_from_input():
+	if Input.is_action_just_pressed("flashlight"): toggle_flashlight()
+	if Input.is_action_just_pressed("dash"): start_dash()
+	if Input.is_action_pressed("fire"): shoot()
+	
+func move_player():
 	var current_player_direction: Vector2 = get_move_direction()
 	var player_velocity: Vector2 = Vector2.ZERO
+	
+	if is_dashing:
+		# Dash in the set direction
+		print("DASHING", dash_time_remaining)
+		
+		player_velocity = dash_direction.normalized() * dash_speed
+		dash_time_remaining -= _current_delta
+	
+		# If dash time runs out, stop dashing
+		if is_zero_approx(abs(dash_time_remaining)) or dash_time_remaining <= COOLDOWN_THRESHOLD:
+			is_dashing = false
+			
+		move_and_collide(player_velocity * _current_delta)
+		return
 
 	if current_player_direction.length() > 0:
 		player_velocity = current_player_direction.normalized() * speed
-		move_and_collide(player_velocity * delta)
+		move_and_collide(player_velocity * _current_delta)
 		Global.update_player_position(self.position)
-		$AnimatedSprite2D.play()
-	else: $AnimatedSprite2D.stop()
+		player_animation.play()
+	else: player_animation.stop()
+
+func start_dash():
+	if is_dashing or dash_cooldown_remaining > COOLDOWN_THRESHOLD:
+		return
+
+	# Only start dash if there's a direction
+	if get_move_direction() != Vector2.ZERO:
+		is_dashing = true
+		dash_time_remaining = dash_duration
+		dash_cooldown_remaining = dash_cooldown
+		dash_direction = get_move_direction()
 
 func toggle_flashlight():
-	if Input.is_action_just_pressed("flashlight"):
-		flashlight_enabled = ! flashlight_enabled
-		$FieldOfView.enabled = flashlight_enabled
+	flashlight_enabled = ! flashlight_enabled
+	$FieldOfView.enabled = flashlight_enabled
 	
 func rotate_field_of_view(direction: Vector2, smoothing_scale: float = 0.1):
 	if ! $FieldOfView.enabled: return
@@ -76,24 +123,26 @@ func rotate_field_of_view(direction: Vector2, smoothing_scale: float = 0.1):
 	$FieldOfView.position = self.position.normalized()
 	$FieldOfView.rotation = lerp_angle($FieldOfView.rotation, direction.angle(), smoothing_scale)
 
-func is_shooting() -> bool:
-	return Input.is_action_pressed("fire") && can_shoot
-
-func shoot(direction: Vector2):
+func shoot():
+	if not can_shoot: return
+	
+	var direction: Vector2 = get_mouse_direction()
+	
 	var bullet_instance = bullet.instantiate()
 	get_tree().root.add_child(bullet_instance)
 	var start_position = (self.position + Vector2(100  * direction.normalized().x, 100 * direction.normalized().y))
 	bullet_instance.start(start_position, direction)
+	
 	start_gun_cooldown()
 
 func set_animation(velocity):
 	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
+		player_animation.animation = "walk"
+		player_animation.flip_v = false
+		player_animation.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+		player_animation.animation = "up"
+		player_animation.flip_v = velocity.y > 0
 
 func get_move_direction():
 	var velocity = Vector2.ZERO
--- scenes/weapons/bullet.gd
+++ scenes/weapons/bullet.gd
@@ -16,21 +16,19 @@ func start(_position: Vector2, _direction: Vector2):
 	rotation = direction.angle()
 
 func _process(delta: float) -> void:
-	#position += (direction.normalized() * speed) + Vector2(delta, delta)
 	move_and_collide(direction * speed)
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
-
-func _on_body_entered(node_body: RigidBody2D) -> void:
-	if (node_body.is_in_group("mobs")): 
-		node_body.hit(damage)
 	queue_free()
 
 func _on_flash_timer_timeout() -> void:
 	$MuzzleFlash.hide()
 
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	if (body.is_in_group("mobs")): 
+		body.hit(damage)
+		queue_free()
+
 	if (body.is_in_group("tiles")):
 		# wall hit effect?
 		queue_free()
